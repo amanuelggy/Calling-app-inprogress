861988f96d38d0b72c631f0d5ae7bf8e4ec45ec1
Amanual <amanual2@gmail.com>
2018-06-02 21:39:20 -0400

    calling app

---
+++ src/main/java/com/amanuel/phoneapp/CallingappApplication.java
@@ -0,0 +1,12 @@
+package com.amanuel.phoneapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CallingappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CallingappApplication.class, args);
+	}
+}
---
+++ src/test/java/com/amanuel/phoneapp/CallingappApplicationTests.java
@@ -0,0 +1,16 @@
+package com.amanuel.phoneapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CallingappApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
